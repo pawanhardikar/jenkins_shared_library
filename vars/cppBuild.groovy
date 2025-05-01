7e14f9861aad404f09477328548561fe8a7df90e
pawanhardikar <pawanhardikar@gmail.com>
2025-05-01 23:09:27 +0530

    Update cppBuild.groovy

--- vars/cppBuild.groovy
+++ vars/cppBuild.groovy
@@ -43,7 +43,7 @@ def call(Map config = [:], Closure body) {
 
         stage('Package Artifact') {
             // Package the main executable
-            archiveArtifacts artifacts: "${artifactPath}" // Ensure artifactPath is used.
+            archiveArtifacts artifacts: "${artifactPattern}" // Use artifactPattern
         }
 
         body()
