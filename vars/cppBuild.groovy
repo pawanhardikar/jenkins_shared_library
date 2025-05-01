518fcb05e9cc8ff8a6254f8ed36923123ea3bed1
pawanhardikar <pawanhardikar@gmail.com>
2025-05-02 00:06:11 +0530

    Update cppBuild.groovy

--- vars/cppBuild.groovy
+++ vars/cppBuild.groovy
@@ -1,27 +1,23 @@
 // vars/cppBuild.groovy
 def call(Map config = [:], Closure body) {
-    // Default values for the configuration
     def buildType = config.buildType ?: 'Release'
-    def cmakeCommand = config.cmakeCommand ?: 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=${buildType}'
+    def cmakeCommand = config.cmakeCommand ?: "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=${buildType}"
     def runTests = config.runTests ?: true
-    def artifactPattern = config.artifactPattern ?: 'build/bin/my_app' // Default artifact pattern
-    def buildDir = config.buildDir ?: 'build' // Added build directory parameter
+    def artifactPattern = config.artifactPattern ?: 'build/bin/my_app'
+    def buildDir = config.buildDir ?: 'build'
 
-    // Helper function to get artifact path
-    def getArtifactPath = { ->
+    def getArtifactPath = {
         def path = artifactPattern
         if (!artifactPattern.contains('/')) {
             path = "${buildDir}/${artifactPattern}"
         }
-        //handle relative paths
         if (artifactPattern.startsWith('.')) {
             path = artifactPattern
         }
         return path
     }
 
-    // Example of using a closure for a block of steps
-    body.resolveStrategy = Closure.DELEGATE_FIRST  // Important for using Jenkins DSL within the closure
+    body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = this
 
     node {
@@ -31,7 +27,6 @@ def call(Map config = [:], Closure body) {
 
         stage('Build') {
             dir('.') {
-                // Configure and build with CMake.
                 bat "${cmakeCommand}"
                 bat "cmake --build ${buildDir} --config ${buildType}"
             }
@@ -44,7 +39,6 @@ def call(Map config = [:], Closure body) {
                         bat 'ctest --verbose'
                     }
                 } catch (Exception e) {
-                    //test failures should not stop the pipeline
                     echo "Tests Failed, check the logs"
                 }
             } else {
@@ -53,7 +47,6 @@ def call(Map config = [:], Closure body) {
         }
 
         stage('Package Artifact') {
-            // Package the main executable
             def artifactPath = getArtifactPath()
             echo "Archiving artifact: ${artifactPath}"
             bat "dir ${buildDir}" // Debug
@@ -66,13 +59,13 @@ def call(Map config = [:], Closure body) {
             }
         }
 
-        // Execute the closure.  This is where the 'createArtifact' call happens.
         body()
     }
 }
 
-def createArtifact(String name, String type) {
+def createArtifact(Map args) {
+    def name = args.name
+    def type = args.type
     echo "Creating artifact: Name: ${name}, Type: ${type}"
-    // In real scenario, we can create a file here
     bat "echo Creating ${name}.${type}"
 }
