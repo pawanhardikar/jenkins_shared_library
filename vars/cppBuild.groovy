f82af76ba4126e2231d3a02b6c3d209b8757ec4e
pawanhardikar <pawanhardikar@gmail.com>
2025-05-01 21:04:48 +0530

    Deleted helloWorld.groovy file and added cppBuild.groovy file

---
+++ vars/cppBuild.groovy
@@ -0,0 +1,55 @@
+// vars/cppBuild.groovy
+def call(Map config = [:], Closure body) {
+    // Default values for the configuration
+    def buildType = config.buildType ?: 'Release'
+    def cmakeCommand = config.cmakeCommand ?: 'cmake ..'
+    def makeCommand = config.makeCommand ?: 'make'
+    def runTests = config.runTests ?: true
+    def artifactPath = config.artifactPath ?: 'build/my_app'
+
+    // Example of using a closure for a block of steps
+    body.resolveStrategy = Closure.DELEGATE_FIRST  // Important for using Jenkins DSL within the closure
+    body.delegate = this
+
+    node {
+        stage('Checkout') {
+            checkout scm
+        }
+
+        stage('Build') {
+            dir('build') {  // Create and enter the 'build' directory
+                sh "${cmakeCommand} -DCMAKE_BUILD_TYPE=${buildType}"
+                sh "${makeCommand}"
+            }
+        }
+
+        stage('Test') {
+            if (runTests) {
+                try {
+                    dir('build') {
+                        sh 'ctest --verbose'
+                    }
+                } catch (Exception e) {
+                    //test failures should not stop the pipeline
+                    echo "Tests Failed, check the logs"
+                }
+            } else {
+                echo "Skipping tests."
+            }
+        }
+
+        stage('Package Artifact') {
+            // Package the main executable
+            archiveArtifacts artifacts: artifactPath
+        }
+
+        // Execute the closure provided by the pipeline
+        body() // Execute the closure
+    }
+}
+
+def createArtifact(String name, String type) {
+    echo "Creating artifact: Name: ${name}, Type: ${type}"
+    // In real scenario, we can create a file here
+    sh "touch ${name}.${type}"
+}
